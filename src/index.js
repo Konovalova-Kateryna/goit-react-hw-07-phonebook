60551759d571b55f6e28b151215c88a6bc7b1b70
Konovalova-Kateryna <kytana2112@gmail.com>
2022-10-11 21:21:28 +0300

    Дубль-2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import store, { persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
